5a3fff20e86b988439caa7c80c8f53038801f8d5
Denis-Agustin-Guerra <46799237+Denis-Agustin-Guerra@users.noreply.github.com>
2023-11-02 00:07:14 -0300

    Cambio de logica al no poder usar la Funcion DataTable (version Final)

--- CalcuInterfaz/Form1.cs
+++ CalcuInterfaz/Form1.cs
@@ -69,8 +69,9 @@ namespace CalcuInterfaz
                 return "0";
             }
 
-            // Reconocer y convertir números negativos
-            entrada = Regex.Replace(entrada, @"(?<=[^\d])-", "0-");
+            // Reconocer y convertir números negativos y valida si el - esta precedido por otro operador
+            entrada = Regex.Replace(entrada, @"(?<=[^\d+\-*/(])-(?=\d)", "0-");
+
 
             // Validaciones
             if (entrada.StartsWith("*") || entrada.StartsWith("/") || entrada.EndsWith("+") || entrada.EndsWith("-") ||
@@ -97,16 +98,158 @@ namespace CalcuInterfaz
             }
 
             // Evaluar la expresión
+            string resultado = EvaluarExpresion(entrada);
+
+            return resultado;
+        }
+        private static string EvaluarExpresion(string expresion)
+        {
             try
             {
-                DataTable dt = new DataTable();
-                var result = dt.Compute(entrada, "");
-                return result.ToString();
+                Stack<decimal> numeros = new Stack<decimal>();
+                Stack<char> operadores = new Stack<char>();
+
+                int i = 0;
+                while (i < expresion.Length)
+                {
+                    char c = expresion[i];
+
+                    if (char.IsDigit(c) || c == '-' && (i == 0 || expresion[i - 1] == '('))
+                    {
+                        int j = i;
+                        while (j < expresion.Length && (char.IsDigit(expresion[j]) || expresion[j] == '.'))
+                        {
+                            j++;
+                        }
+
+                        string numeroStr = expresion.Substring(i, j - i);
+                        decimal numero = decimal.Parse(numeroStr, CultureInfo.InvariantCulture);
+                        numeros.Push(numero);
+                        i = j;
+                    }
+                    else if (c == '+' || c == '-' || c == '*' || c == '/')
+                    {
+                        if (i == 0 || expresion[i - 1] == '(')
+                        {
+                            return "Syntax error"; // Operador incorrecto después de (
+                        }
+                        else if (i > 0 && (expresion[i - 1] == '+' || expresion[i - 1] == '-' || expresion[i - 1] == '*' || expresion[i - 1] == '/'))
+                        {
+                            return "Syntax error"; // Dos operadores juntos
+                        }
+
+                        while (operadores.Count > 0 && Precedencia(operadores.Peek()) >= Precedencia(c))
+                        {
+                            decimal b = numeros.Pop();
+                            decimal a = numeros.Pop();
+                            char operador = operadores.Pop();
+                            decimal resultado = AplicarOperador(a, b, operador);
+                            numeros.Push(resultado);
+                        }
+                        operadores.Push(c);
+                        i++;
+                    }
+                    else if (c == '(')
+                    {
+                        operadores.Push(c);
+                        i++;
+                    }
+                    else if (c == ')')
+                    {
+                        while (operadores.Count > 0 && operadores.Peek() != '(')
+                        {
+                            decimal b = numeros.Pop();
+                            decimal a = numeros.Pop();
+                            char operador = operadores.Pop();
+                            decimal resultado = AplicarOperador(a, b, operador);
+                            numeros.Push(resultado);
+                        }
+                        if (operadores.Count == 0 || operadores.Peek() != '(')
+                        {
+                            return "Syntax error"; // Falta el paréntesis de apertura correspondiente
+                        }
+                        operadores.Pop(); // Quitar el paréntesis de apertura
+                        i++;
+                    }
+                    else if (char.IsWhiteSpace(c))
+                    {
+                        // Ignorar espacios en blanco
+                        i++;
+                    }
+                    else
+                    {
+                        return "Syntax error";
+                    }
+                }
+
+                while (operadores.Count > 0)
+                {
+                    decimal b = numeros.Pop();
+                    decimal a = numeros.Pop();
+                    char operador = operadores.Pop();
+                    decimal resultado = AplicarOperador(a, b, operador);
+                    numeros.Push(resultado);
+                }
+
+                if (numeros.Count == 1 && operadores.Count == 0)
+                {
+                    decimal resultado = numeros.Pop();
+                    if (resultado % 1 == 0)
+                    {
+                        return resultado.ToString("N0"); // Mostrar como entero
+                    }
+                    else
+                    {
+                        return resultado.ToString("N2"); // Mostrar con un decimal
+                    }
+                }
+                else
+                {
+                    return "Syntax error";
+                }
+
             }
             catch (Exception)
             {
                 return "Math error";
             }
         }
+
+        private static int Precedencia(char operador)
+        {
+            switch (operador)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private static decimal AplicarOperador(decimal a, decimal b, char operador)
+        {
+            switch (operador)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b == 0)
+                    {
+                        return 0;
+                    }
+                    return a / b;
+                default:
+                    throw new ArgumentException("Operador no válido: " + operador);
+            }
+        }
+
     }
 }
